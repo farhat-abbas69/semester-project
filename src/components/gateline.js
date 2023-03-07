a54461fd8234ec6a0d567fa9c08ad061f517edae
amoeed303 <200842@students.au.edu.pk>
2023-03-07 21:28:26 +0500

    some UI changes

--- src/components/gateline.js
+++ src/components/gateline.js
@@ -1,8 +1,14 @@
 import './styles/gateline.css'
+import { useState } from 'react';
 const GateLine = ()=>{
+    const [bit, setBit] = useState(0);
+    let clickHandler=(event)=>{
+       setBit(event.target.value(1));
+    }
     return(
         <div className="gateline">
-            <h1 className='h1'>{'|0>'}</h1>
+            <h1 className='h1' onClick={clickHandler} >{`|`}{bit}{`>`}</h1>
+            
             <span className='row-line'></span>
         </div>
     )
